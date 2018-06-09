0c8f26e6e715811c04a4c8f130623b9ca374119b
Matthew Piestrak <mpiestrak@gmail.com>
2018-06-09 23:14:00 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,13 @@
 Class Person
-def name=(person_name)
-  @name = person_name
-end
-
 def name
   @name
 end
 
-def job=(person_job)
-  @job = person_job
+def name =(name)
+  @name = name
+end
+def job=(title)
+  @job = title
 end
 
 def job
